ae71fbfdaff7f6324fc44ea2c93147c03bf21699
abdullah <xnm555@gmail.com>
2020-07-23 14:08:11 +0300

    modifying the accordion functionality

--- src/loaders/accordion.js
+++ src/loaders/accordion.js
@@ -7,10 +7,16 @@ accordion.forEach(el => {
         const target = e.target.closest(".accordion__item");
         const barIcon = target.querySelector(".accordion__icon");
         const content = target.querySelector(".accordion__content");
-        Array.from(document.querySelectorAll(".accordion__content")).forEach(el => el.classList.remove("accordion__content--open"));
-        Array.from(document.querySelectorAll(".accordion__icon")).forEach(el => el.classList.remove("accordion__icon--open"));
-        content.classList.toggle("accordion__content--open");
-        barIcon.classList.toggle("accordion__icon--open");
+        accordion.forEach(el => {
+            if (target === el) {
+                content.classList.toggle("accordion__content--open");
+                barIcon.classList.toggle("accordion__icon--open");
+            } else {
+                el.querySelector(".accordion__content").classList.remove("accordion__content--open");
+                el.querySelector(".accordion__icon").classList.remove("accordion__icon--open");
+
+            }
+        })
     })
     
     
